e8a2d37211b0ffdabc9e7f24d00873de692b6efb
bulelife <thebulelife@163.com>
2016-03-27 00:23:15 +0800

    add item dir and infomartion

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect);
+
+var DEST = './dist/js';
+gulp.task('default',function(){
+    return gulp.src('./src/js/angular.js')
+    .pipe(gulp.dest(DEST))
+    .pipe(uglify())
+    .pipe(rename({extname:'.min.js'}))
+    .pipe(gulp.dest(DEST));
+});
+gulp.task('webserver',function(){
+    connect.server();
+});
+
+gulp.task('default',['webserver']);
